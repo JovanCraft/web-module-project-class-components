4b3c06d468f0daaa7f8dab4e34dcf01182fb9018
jovan <jovancraft@gmail.com>
2023-10-04 20:43:07 -0500

    added onChange and onSubmit to the Form

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onSubmit = evt => {
+    evt.preventDefault()
+    const { addTodo } = this.props
+    addTodo(this.state.name)
+  }
+
+  onChange = evt => {
+    const { value } = evt.target
+    this.setState({
+      name: value
+    })
+  }
+
   render() {
+
     return (
-      <div>
-        Form
-      </div>
+      <form onSubmit={this.onSubmit}>
+        <input type='text' value={this.state.name} onChange={this.onChange}/>
+        <input type='submit' />
+      </form>
     )
   }
 }
